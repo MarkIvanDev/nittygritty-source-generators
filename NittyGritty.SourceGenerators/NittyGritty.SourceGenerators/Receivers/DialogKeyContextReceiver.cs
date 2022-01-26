ed0a5b2b8a03f3cc1087666f9d60a3873c2b46c0
Mark Ivan Basto <markivan.developer@outlook.com>
2022-01-26 20:26:44 +0800

    add nittygritty source generators

---
+++ NittyGritty.SourceGenerators/NittyGritty.SourceGenerators/Receivers/DialogKeyContextReceiver.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using NittyGritty.SourceGenerators.Annotations;
+using NittyGritty.SourceGenerators.Helpers;
+
+namespace NittyGritty.SourceGenerators.Receivers
+{
+    internal class DialogKeyContextReceiver : ISyntaxContextReceiver
+    {
+        public List<INamedTypeSymbol> Classes { get; } = new List<INamedTypeSymbol>();
+
+        public void OnVisitSyntaxNode(GeneratorSyntaxContext context)
+        {
+            if (context.Node is ClassDeclarationSyntax vm)
+            {
+                if (context.SemanticModel.GetDeclaredSymbol(vm) is INamedTypeSymbol vmSymbol &&
+                    vmSymbol.GetAttribute<DialogKeyAttribute>() != null)
+                {
+                    Classes.Add(vmSymbol);
+                }
+            }
+        }
+    }
+}
